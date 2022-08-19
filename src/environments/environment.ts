ce37d861d00f5b2a05d26cf54e96d8a453b34821
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-08-19 16:40:10 -0400

    add local environment variable pointing to spring boot api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,6 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
+export const url = `http://localhost:5000/api`; // this is where our Spring Boot API is running
