7c7b03aaa9b08a3476dd3d6211acc32e4c19db1c
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-08-19 16:33:16 -0400

    add service and all components for demo w/o security

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AllComponent } from './components/all/all.component';
+import { MainComponent } from './components/main/main.component';
+import { FindComponent } from './components/find/find.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AllComponent,
+    MainComponent,
+    FindComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
